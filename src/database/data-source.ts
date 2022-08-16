743a64ea4e42b275c35410e3f00b131fe9e92b2c
Desector123456 <coronadoc899@gmail.com>
2022-08-16 17:33:15 -0400

    [FIX]: Cambio de version de librerias

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: "localhost",
-  username: "root",
+  host: 'localhost',
+  username: 'root',
   port: 3306,
-  database: "relojcontrol",
-  password: "docker",
+  database: 'relojcontrol',
+  password: 'Cinet.2022%%',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   //subscribers: [__dirname + '/../**/*.subscriber.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{ts,js}'],
